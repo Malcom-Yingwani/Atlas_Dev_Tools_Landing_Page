398fbb0cf8f9ff8e31123c84a134db0c34d9bbc8
Malcom-Yingwani <nsovomalcom@gmail.com>
2026-01-08 19:12:15 +0200

    made the app responsive and interactive

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// copyright logic
 document.getElementById("copyright-year").textContent = new Date().getFullYear();
+
+
+// email submission
+
+const joinButton = document.getElementById('join_button');
+    const emailField = document.getElementById('email_field');
+    const successMessage = document.getElementById('success_message')
+
+    if (joinButton && emailField && successMessage) {
+      
+        
+        joinButton.addEventListener('click', function() {
+            const email = emailField.value.trim();
+            
+            if (email) {
+                // Clear the email field
+                emailField.value = "successMessage.style.display = ''";
+                
+                // Show success message
+                successMessage.style.display = '';
+                
+                // Hide message after 4 seconds
+                setTimeout(function() {
+                    successMessage.style.display = 'none';
+                }, 4000);
+                
+                console.log('Email submitted:', email);
+            }
+        });
+    }
